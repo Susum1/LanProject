8693ae98ff274eb46b03fdc920ced2d9cb3e57e3
Susum1 <vova.zaycev.95@gmail.com>
2026-07-19 13:51:30 +0300

    Add files via upload

---
+++ MineLan/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using MineLAN.Controllers;
+
+namespace MineLAN;
+
+public partial class MainWindow : Window
+{
+    private readonly MainWindowController _controller;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        _controller = new MainWindowController(this);
+
+        _controller.Initialize();
+    }
+
+    private void CreateProfileButton_Click(object sender, RoutedEventArgs e)
+    {
+        _controller.CreateProfile();
+    }
+
+    private void DeleteProfileButton_Click(object sender, RoutedEventArgs e)
+    {
+        _controller.DeleteSelectedProfile();
+    }
+}
