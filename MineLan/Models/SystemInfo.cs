3e91b88c0a94b6aafdbfd40b0e878226f165b042
Susum1 <vova.zaycev.95@gmail.com>
2026-07-19 12:40:22 +0300

    Add files via upload

---
+++ MineLan/Models/SystemInfo.cs
@@ -0,0 +1,20 @@
+﻿namespace MineLAN.Models;
+
+public class SystemInfo
+{
+    public string UserName { get; set; } = "";
+
+    public string MachineName { get; set; } = "";
+
+    public string WindowsVersion { get; set; } = "";
+
+    public bool HasInternet { get; set; }
+
+    public bool IsAdministrator { get; set; }
+
+    public bool WireGuardInstalled { get; set; }
+
+    public string WireGuardVersion { get; set; } = "";
+
+    public string? WireGuardPath { get; set; }
+}
---
+++ MineLan/Models/WireGuardProfile.cs
@@ -0,0 +1,22 @@
+﻿namespace MineLAN.Models;
+
+public class WireGuardProfile
+{
+    public string Name { get; set; } = "";
+
+    public string PrivateKey { get; set; } = "";
+
+    public string PublicKey { get; set; } = "";
+
+    public string Address { get; set; } = "";
+
+    public string DNS { get; set; } = "1.1.1.1";
+
+    public string Endpoint { get; set; } = "";
+
+    public string AllowedIPs { get; set; } = "0.0.0.0/0";
+
+    public int ListenPort { get; set; } = 51820;
+
+    public string PresharedKey { get; set; } = "";
+}
