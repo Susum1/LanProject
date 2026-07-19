1d0d64411e1f1644dd3a31d6be9e1109a2996332
Susum1 <vova.zaycev.95@gmail.com>
2026-07-19 13:46:41 +0300

    Add files via upload

---
+++ MineLan/Models/ProfileCreateModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MineLAN.Models;
+
+public class ProfileCreateModel
+{
+    public string Name { get; set; } = "";
+
+    public string Address { get; set; } = "";
+
+    public string Endpoint { get; set; } = "";
+
+    public string DNS { get; set; } = "1.1.1.1";
+}
